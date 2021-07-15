3814506f0c298cbc86ff63610791d401a7e69310
wuhaoqiang <wuhaoqiang5@gmail.com>
2021-07-15 17:00:21 +0800

    init

---
+++ src/main/java/com/example/mavendemo/web/WebController.java
@@ -0,0 +1,21 @@
+package com.example.mavendemo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wuhaoqiang
+ * @title: WebController
+ * @projectName mavendemo
+ * @description: TODO
+ * @date 2021/7/15 16:47
+ */
+@RestController
+public class WebController {
+
+    @RequestMapping("/kevin")
+    public String index() {
+        return "Hello kevin!";
+    }
+
+}
